6201a4c38d6b83665dcd3774b7fe9cdbb2caaa76
Chidimma Phoebe Echebiri <chidimma.echebiri@inn.no>
2024-08-10 02:49:16 +0200

    updated the metadata files to those  downloaded from SRA

--- R/metadata_public_data.R
+++ R/metadata_public_data.R
@@ -5,24 +5,26 @@ library(tidyverse)
 #Load and clean the first public data
 #This contains pre and postexercise (RT) data
 #see article https://pubmed.ncbi.nlm.nih.gov/28273480/
-SRP102542 <- read_csv("public_data/sra_result-SRP102542.csv") %>%
-  select("Experiment Accession", "Experiment Title", "Sample Accession") %>%
-  separate("Experiment Title", c("Title", "Group", "Age", "Exercise", "Time"))
+#Metadata downloaded from https://www.ncbi.nlm.nih.gov/Traces/study/?page=2&acc=gse97084&o=acc_s%3Aa
+SRP102542 <- read_csv("data/SRP102542.txt") %>%
+  select("Run", "AGE", "biopsy_timepoint", "exercise_type") 
+#saveRDS(SRP102542, "./data/SRP102542_metadata.RDS")
 
+colnames(SRP102542)
 
-unique(SRP102542$Time)
 #Load second public data
 
 
-#The following is baseline transcriptome metadata. But does not specifiy age
+#The following is baseline transcriptome metadata.
 #See here https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE58608 
 #Participants were young see https://faseb.onlinelibrary.wiley.com/doi/epdf/10.1096/fj.14-255000
-SRP043368 <- read_csv("public_data/sra_result-SRP043368.csv")%>%
-  select("Experiment Accession", "Experiment Title", "Sample Accession") %>%
-  separate("Experiment Title", c("Title", "Group"))
+#metadata downloaded from https://www.ncbi.nlm.nih.gov/Traces/study/?page=2&acc=gse58608&o=acc_s%3Aa
+SRP043368 <- read_csv("data/SRP043368.txt")%>%
+  select("Run", "gender", "exercise_status") 
+#saveRDS(SRP043368, "data/SRP043368_metadata.RDS")
 
 
-unique(SRP043368$`Experiment Title`)
+colnames(SRP043368)
 #Load third public data
 
 
@@ -31,9 +33,14 @@ unique(SRP043368$`Experiment Title`)
 #Contains only baseline of young individuals
 
 #see article https://pubmed.ncbi.nlm.nih.gov/33071237/
+#metadata downloaded from https://www.ncbi.nlm.nih.gov/Traces/study/?page=2&acc=GSE157585&o=acc_s%3Aa
 
-SRP280348 <- read_csv("public_data/sra_result-SRP280348.csv")%>%
-  select("Experiment Accession", "Experiment Title", "Sample Accession") %>%
-  separate("Experiment Title", c("Title", "Group", "Age"), sep = " ")
+#biopsy 1 is preexercise in old, while biopsy 0 is baseline young
+SRP280348 <- read_csv("data/SRP280348.txt")%>%
+  select("Run", "biopsy", "study_arm") 
 
-SRP280348$Age
+#saveRDS(SRP280348, "data/SRP280348_metadata.RDS")
+
+colnames(SRP280348)
+
+unique(SRP280348$biopsy)
